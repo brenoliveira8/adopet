84f1ec56ba743c90234917744027931136fa9294
Breno Mascarenhas <brenoliveira8@gmail.com>
2023-04-13 15:48:43 -0300

    :heavy_plus_sign: build: Added Validation Dependency

---
+++ src/main/java/br/com/mascarenhasb2/adopet/controller/GuardianController.java
@@ -0,0 +1,11 @@
+package br.com.mascarenhasb2.adopet.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tutores")
+public class GuardianController {
+
+
+}
